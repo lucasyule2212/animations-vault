813261c1dfeb6816f982aa212ddddb8ef8a7e25c
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-10 20:04:53 -0300

    feat(header-section): adding header-section component

---
+++ src/app/(home)/components/header-section.tsx
@@ -0,0 +1,15 @@
+const HeaderSection = () => {
+  return (
+    <section className="px-52">
+      <header>
+        <h1>Animations Vault</h1>
+      </header>
+      <p className="flex items-end gap-1 font-serif italic text-zinc-400">
+        Building some cool stuff for study and inspiration.
+      </p>
+      <footer className="mt-4 text-sm text-zinc-400">by Lucas Yule</footer>
+    </section>
+  )
+}
+
+export default HeaderSection
