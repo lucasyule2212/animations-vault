0fe08843d053b7b02c20fb6c54681bdfafd9f048
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-10 00:08:26 -0300

    chore(tailwind.config): extending zinc strongest shades

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="text-accent">Hello World</div>
+  return (
+    <div className="text-accent mx-2 mt-2 rounded-md border border-zinc-800 bg-zinc-950 p-10">
+      Hello World
+    </div>
+  )
 }
