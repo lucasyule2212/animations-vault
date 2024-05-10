e61d0f3c2860527a24276322c953eb1b29edc12e
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-10 20:05:16 -0300

    feat(home-page): adding home-page folder + base component

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import HeaderSection from './components/header-section'
+
+export default function Home() {
+  return (
+    <main className="flex w-screen flex-col rounded-md px-56 py-32 text-base text-accent">
+      <HeaderSection />
+    </main>
+  )
+}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="text-accent mx-2 mt-2 rounded-md border border-zinc-800 bg-zinc-950 p-10">
-      Hello World
-    </div>
-  )
-}
