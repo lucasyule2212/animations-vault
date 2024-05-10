c8e5b5c744ddb7f2f9037405f9002dc344865e0c
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-09 23:58:28 -0300

    feat(shadcn): adding shadcn-ui + css base configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,29 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Animations Vault",
-  description: "A collection of animations for study and inspiration.",
-};
+  title: 'Animations Vault',
+  description: 'A collection of animations for study and inspiration.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(inter.className, 'min-h-screen bg-zinc-900 antialiased')}
+      >
+        {children}
+      </body>
     </html>
-  );
+  )
 }
