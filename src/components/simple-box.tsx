594d45ec05518d183a5dc3bc3062a3a0d9f70eaa
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-12 20:48:07 -0300

    refactor(simple-box): turning simple-box component to motion.div

--- src/components/simple-box.tsx
+++ src/components/simple-box.tsx
@@ -1,5 +1,14 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
 const SimpleBox = () => {
-  return <div className="h-12 w-12 rounded-full border bg-blue-400 shadow-lg" />
+  return (
+    <motion.div
+      layout
+      className="h-12 w-12 rounded-full border bg-blue-400 shadow-lg"
+    />
+  )
 }
 
 export default SimpleBox
