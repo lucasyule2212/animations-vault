c6061dad33c6e09b58621a0ea7232e3bf9ccd50b
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-14 12:54:14 -0300

    refactor(ease-component): adding easingClass prop + layout control

--- src/components/easing-animations/ease-component.tsx
+++ src/components/easing-animations/ease-component.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { ArrowLeft, ArrowRight } from 'lucide-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { cn } from '@/lib/utils'
 
@@ -10,21 +10,29 @@ import { Card } from '../ui/card'
 
 type EaseComponentProps = {
   easing?: 'ease-in' | 'ease-out' | 'ease-in-out' | 'linear'
+  justifyAll?: 'start' | 'end'
 }
 
-const EaseComponent = ({ easing = 'linear' }: EaseComponentProps) => {
-  const [justify, setJustify] = useState('start')
+const EaseComponent = ({
+  easing = 'linear',
+  justifyAll = 'start',
+}: EaseComponentProps) => {
+  const [justify, setJustify] = useState(justifyAll)
+
+  useEffect(() => {
+    setJustify(justifyAll)
+  }, [justifyAll])
 
   const justifyClass: Record<string, string> = {
     start: 'justify-start',
     end: 'justify-end',
   }
 
-  const easingClass: Record<string, string> = {
-    'ease-in': 'ease-in-circ',
-    'ease-out': 'ease-out-circ',
-    'ease-in-out': 'ease-in-out-circ',
-    linear: 'ease-linear',
+  const easingClass: Record<string, number[]> = {
+    'ease-in': [0.55, 0.055, 0.675, 0.19], // -- ease-in-cubic
+    'ease-out': [0.23, 1, 0.32, 1], // -- ease-out-quint
+    'ease-in-out': [0.785, 0.135, 0.15, 0.86], // -- ease-in-out-circ
+    linear: [0, 0, 1, 1],
   }
 
   return (
@@ -40,14 +48,8 @@ const EaseComponent = ({ easing = 'linear' }: EaseComponentProps) => {
         >
           <ArrowLeft className="h-4 w-4 text-zinc-700" />
         </Button>
-        <Card
-          className={cn(
-            'flex w-full p-6',
-            justifyClass[justify],
-            easingClass[easing],
-          )}
-        >
-          <SimpleBox />
+        <Card className={cn('flex w-full p-6', justifyClass[justify])}>
+          <SimpleBox easing={easingClass[easing]} />
         </Card>
         <Button
           variant="ghost"
