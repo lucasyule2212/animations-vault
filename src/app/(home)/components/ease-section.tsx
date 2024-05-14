55843cfef4cf7084415faa840aeb61db162ffea5
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-14 12:54:38 -0300

    refactor(ease-section): adding global justify control + play all button

--- src/app/(home)/components/ease-section.tsx
+++ src/app/(home)/components/ease-section.tsx
@@ -1,15 +1,33 @@
+'use client'
+import { Play } from 'lucide-react'
+import { useState } from 'react'
+
 import EaseComponent from '@/components/easing-animations/ease-component'
+import { Button } from '@/components/ui/button'
 
 const EaseSection = () => {
+  const [justifyAll, setJustifyAll] = useState<'start' | 'end'>('start')
+
+  function handleToggleJustifyAll() {
+    setJustifyAll((prev) => (prev === 'start' ? 'end' : 'start'))
+  }
   return (
     <section className="mt-24 flex flex-col gap-4 px-52">
-      <header>
+      <header className="flex items-center  gap-4">
         <h2>Easing</h2>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={handleToggleJustifyAll}
+          className="h-8 w-8 rounded-full"
+        >
+          <Play className="h-4 w-4 text-zinc-700" />
+        </Button>
       </header>
-      <EaseComponent easing="linear" />
-      <EaseComponent easing="ease-in" />
-      <EaseComponent easing="ease-out" />
-      <EaseComponent easing="ease-in-out" />
+      <EaseComponent easing="linear" justifyAll={justifyAll} />
+      <EaseComponent easing="ease-in" justifyAll={justifyAll} />
+      <EaseComponent easing="ease-out" justifyAll={justifyAll} />
+      <EaseComponent easing="ease-in-out" justifyAll={justifyAll} />{' '}
     </section>
   )
 }
