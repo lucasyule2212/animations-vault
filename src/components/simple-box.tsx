95ea4ffeb4ddd216f997d7c5fff95086f5f452c7
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-14 12:53:12 -0300

    refactor(simple-box): adding transition prop + custom easing

--- src/components/simple-box.tsx
+++ src/components/simple-box.tsx
@@ -2,10 +2,18 @@
 
 import { motion } from 'framer-motion'
 
-const SimpleBox = () => {
+type SimpleBoxProps = {
+  easing?: number[]
+}
+
+const SimpleBox = ({ easing }: SimpleBoxProps) => {
   return (
     <motion.div
       layout
+      transition={{
+        duration: 1,
+        ease: easing,
+      }}
       className="h-12 w-12 rounded-full border bg-blue-400 shadow-lg"
     />
   )
