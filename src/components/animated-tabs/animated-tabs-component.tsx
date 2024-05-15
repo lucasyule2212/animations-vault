b5f0f5d0173671eb282470a2ce8a9537bc7dc65d
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-15 15:45:24 -0300

    fix(animated-tabs): fixing component responsivity

--- src/components/animated-tabs/animated-tabs-component.tsx
+++ src/components/animated-tabs/animated-tabs-component.tsx
@@ -40,8 +40,8 @@ const AnimatedTabsComponent = () => {
   return (
     <Card className="flex w-full min-w-[300px] items-center justify-center p-6">
       <div
-        className="h-[54px] min-w-[450px] rounded-2xl bg-zinc-900 p-2 
-    shadow-[inset_0px_0.5px_0px_1px_#65656522] drop-shadow-md"
+        className="rounded-2xl bg-zinc-900 p-2 shadow-[inset_0px_0.5px_0px_1px_#65656522] drop-shadow-md 
+    md:h-[54px] md:min-w-[450px]"
         onMouseLeave={() => setActiveTab('')}
       >
         <div className="flex h-full w-full flex-col items-center justify-evenly md:flex-row">
@@ -50,7 +50,7 @@ const AnimatedTabsComponent = () => {
               key={tab.text}
               layout
               className={cn(
-                'relative flex cursor-pointer items-center gap-2 px-4 py-2 text-sm outline-none transition-colors',
+                'relative flex w-full cursor-pointer items-center gap-2 px-4 py-2 text-sm outline-none transition-colors',
                 activeTab === tab.text ? tab.textColor : 'text-zinc-500',
               )}
               onFocus={() => setActiveTab(tab.text)}
@@ -67,7 +67,9 @@ const AnimatedTabsComponent = () => {
                 />
               ) : null}
               <tab.icon className="h-3 w-3" />
-              <span className="relative text-inherit">{tab.text}</span>
+              <span className="relative text-nowrap text-inherit">
+                {tab.text}
+              </span>
             </motion.div>
           ))}
         </div>
