7d8cbe96bc20e77a5d7534b670ab7f897ff59507
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-16 00:01:14 -0300

    refactor(next.config): adding image remotePatterns config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname:
+          'animations-on-the-web-git-how-i-use-3066e1-emilkowalski-s-team.vercel.app',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
