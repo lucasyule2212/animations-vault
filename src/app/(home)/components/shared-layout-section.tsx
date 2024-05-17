405b9f2abf85b47e000914dcd0615fafc20db4ab
Lucas Yule <lyrma@cin.ufpe.br>
2024-05-17 18:40:34 -0300

    refactor(shared-layout-section): updating component name

--- src/app/(home)/components/shared-layout-section.tsx
+++ src/app/(home)/components/shared-layout-section.tsx
@@ -4,7 +4,7 @@ const SharedLayoutSection = () => {
   return (
     <section className="mt-24 flex flex-col gap-4 sm:px-16 md:px-20 lg:px-32 xl:px-52">
       <header className="flex items-center  gap-4">
-        <h2>Apple store-like list</h2>
+        <h2>Apple Store-like list</h2>
       </header>
       <SharedLayoutComponent />
     </section>
