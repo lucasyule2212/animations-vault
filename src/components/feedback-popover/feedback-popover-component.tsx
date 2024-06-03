b0b8d28a195ca3e7437bf7eb27459a10647c6e73
Lucas Yule <lyrma@cin.ufpe.br>
2024-06-02 21:49:04 -0300

    refactor(feedback-popover): adding success state visual feedback

--- src/components/feedback-popover/feedback-popover-component.tsx
+++ src/components/feedback-popover/feedback-popover-component.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { AnimatePresence, motion } from 'framer-motion'
+import { CheckCircle } from 'lucide-react'
 import { useEffect, useRef, useState } from 'react'
 import { useOnClickOutside } from 'usehooks-ts'
 
@@ -45,7 +46,7 @@ const FeedbackPopoverComponent = () => {
     setTimeout(() => {
       setSubmissionState('idle')
       resetStates()
-    }, 5000)
+    }, 6000)
   }
 
   return (
@@ -59,39 +60,84 @@ const FeedbackPopoverComponent = () => {
         </Button>
       </motion.span>
 
-      <AnimatePresence initial={false}>
+      <AnimatePresence initial={false} mode="popLayout">
         {isOpen && (
           <motion.div
             ref={ref}
-            className="absolute h-[192px] w-[364px] rounded-xl border border-zinc-300 bg-zinc-100 p-[6px] text-accent drop-shadow-sm"
+            className="absolute h-[192px] w-[364px] overflow-hidden rounded-xl border border-zinc-300 bg-zinc-100 p-[6px] text-accent drop-shadow-sm"
             layoutId="feedback-root-container"
           >
-            <motion.div className="relative flex h-full w-full flex-col rounded-lg border border-zinc-300 bg-white px-1 py-1">
-              <motion.p
-                layoutId="feedback-root-text"
-                data-hasfeedback={!!feedback}
-                className="absolute left-4 top-3 text-sm text-zinc-600 data-[hasfeedback=true]:text-opacity-0"
+            {submissionState === 'success' ? (
+              <motion.div
+                key="feedback-root-success"
+                initial={{
+                  y: -32,
+                  opacity: 0,
+                  filter: 'blur(4px)',
+                }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                  filter: 'blur(0px)',
+                }}
+                exit={{
+                  y: 32,
+                  opacity: 0,
+                  filter: 'blur(4px)',
+                }}
+                transition={{
+                  type: 'spring',
+                  duration: 0.8,
+                  bounce: 0,
+                }}
+                className="absolute inset-0 z-20 flex h-full w-full flex-col items-center justify-center gap-4 rounded-xl bg-zinc-100"
               >
-                Feedback
-              </motion.p>
-              <Textarea
-                className="mb-auto h-[70%] w-full border-none bg-white outline-none ring-0 ring-offset-0 focus:border-none focus:outline-none focus:ring-0"
-                value={feedback}
-                onChange={(e) => setFeedback(e.target.value)}
-                autoFocus
-              />
-              <div className="absolute -left-1 bottom-11 z-10 h-2 w-2 rounded-r-full border border-zinc-300 bg-zinc-100" />
-              <div className="absolute -left-[4.5px] bottom-11 z-20 h-2 w-1  bg-zinc-100" />
-              <div className="absolute bottom-12 left-0 w-full border-t border-dashed border-zinc-200" />
-              <div className="absolute -right-1 bottom-11 z-10 h-2 w-2 rounded-l-full border border-zinc-300 bg-zinc-100" />
-              <div className="absolute -right-[4.5px] bottom-11 z-20 h-2 w-1  bg-zinc-100" />
-              <div className="flex h-10 w-full items-center justify-end px-2 py-2">
-                <SubmissionButton
-                  submissionState={submissionState}
-                  handleSubmit={handleSubmit}
+                <CheckCircle className="h-6 w-6 text-green-500" />
+                <div className="space-y-1 text-center">
+                  <p className="text-sm">
+                    <motion.span layoutId="feedback-root-text">
+                      Feedback
+                    </motion.span>{' '}
+                    sent!
+                  </p>
+                  <p className="text-xs text-zinc-500">
+                    Thanks for helping me improve my animations.
+                  </p>
+                </div>
+              </motion.div>
+            ) : (
+              <motion.div
+                key="feedback-root-text"
+                exit={{ y: 8, opacity: 0, filter: 'blur(4px)' }}
+                transition={{ type: 'spring', duration: 0.2, bounce: 0 }}
+                className="relative flex h-full w-full flex-col rounded-lg border border-zinc-300 bg-white px-1 py-1"
+              >
+                <motion.p
+                  layoutId="feedback-root-text"
+                  data-hasfeedback={!!feedback}
+                  className="absolute left-4 top-3 text-sm text-zinc-600 data-[hasfeedback=true]:text-opacity-0"
+                >
+                  Feedback
+                </motion.p>
+                <Textarea
+                  className="mb-auto h-[70%] w-full border-none bg-white outline-none ring-0 ring-offset-0 focus:border-none focus:outline-none focus:ring-0"
+                  value={feedback}
+                  onChange={(e) => setFeedback(e.target.value)}
+                  autoFocus
                 />
-              </div>
-            </motion.div>
+                <div className="absolute -left-1 bottom-11 z-10 h-2 w-2 rounded-r-full border border-zinc-300 bg-zinc-100" />
+                <div className="absolute -left-[4.5px] bottom-11 z-20 h-2 w-1  bg-zinc-100" />
+                <div className="absolute bottom-12 left-0 w-full border-t border-dashed border-zinc-200" />
+                <div className="absolute -right-1 bottom-11 z-10 h-2 w-2 rounded-l-full border border-zinc-300 bg-zinc-100" />
+                <div className="absolute -right-[4.5px] bottom-11 z-20 h-2 w-1  bg-zinc-100" />
+                <div className="flex h-10 w-full items-center justify-end px-2 py-2">
+                  <SubmissionButton
+                    submissionState={submissionState}
+                    handleSubmit={handleSubmit}
+                  />
+                </div>
+              </motion.div>
+            )}
           </motion.div>
         )}
       </AnimatePresence>
