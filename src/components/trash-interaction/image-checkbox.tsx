8c98277a5c33349e85a5d4f8a8a6415eb726b70e
Lucas Yule <lyrma@cin.ufpe.br>
2024-06-09 14:14:18 -0300

    feat(trash-interaction): isolating image-checkbox + adding css animation

---
+++ src/components/trash-interaction/image-checkbox.tsx
@@ -0,0 +1,38 @@
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
+
+import { Checkbox } from '../ui/checkbox'
+
+interface ImageCheckboxProps {
+  image: string
+  onClick: () => void
+}
+
+const ImageCheckbox = ({ image, onClick }: ImageCheckboxProps) => {
+  const [checked, setChecked] = useState(false)
+
+  const handleOnClick = () => {
+    setChecked((prev) => !prev)
+    onClick()
+  }
+
+  return (
+    <div className="cursor-pointer" onClick={handleOnClick}>
+      <Checkbox
+        checked={checked}
+        onChange={() => handleOnClick()}
+        className="absolute right-2 top-2 rounded-full transition duration-200 ease-out data-[state=checked]:border-black data-[state=checked]:bg-black data-[state=checked]:text-white"
+      />
+      <Image
+        src={`https://animations-on-the-web-git-how-i-use-3066e1-emilkowalski-s-team.vercel.app/how-i-use-framer-motion/why-framer-motion/${image}.webp`}
+        width={500}
+        height={500}
+        alt={image}
+        className="h-24 w-24 rounded-xl"
+      />
+    </div>
+  )
+}
+
+export default ImageCheckbox
--- src/components/trash-interaction/trash-interaction-component.tsx
+++ src/components/trash-interaction/trash-interaction-component.tsx
@@ -1,25 +1,35 @@
 'use client'
 
-import Image from 'next/image'
+import { useState } from 'react'
 
 import { Card } from '../ui/card'
-import { Checkbox } from '../ui/checkbox'
+import ImageCheckbox from './image-checkbox'
 
 const IMAGES = ['japan', 'jungle', 'new-york', 'desert']
 
 const TrashInteractionComponent = () => {
+  const [selectedImages, setSelectedImages] = useState<string[]>([])
+
+  const handleImageClick = (image: string) => {
+    setSelectedImages((prev) => {
+      if (prev.includes(image)) {
+        return prev.filter((prevImage) => prevImage !== image)
+      }
+
+      return [...prev, image]
+    })
+  }
+
+  console.log(selectedImages)
+
   return (
     <Card className="flex min-h-[500px] w-full min-w-[300px] items-center justify-center bg-white p-6">
       <div className="flex w-[300px] flex-wrap items-center justify-center gap-4">
         {IMAGES.map((image) => (
           <div key={image} className="relative">
-            <Checkbox className="absolute right-2 top-2 rounded-full data-[state=checked]:border-black data-[state=checked]:bg-black data-[state=checked]:text-white" />
-            <Image
-              src={`https://animations-on-the-web-git-how-i-use-3066e1-emilkowalski-s-team.vercel.app/how-i-use-framer-motion/why-framer-motion/${image}.webp`}
-              width={500}
-              height={500}
-              alt={image}
-              className="h-24 w-24 rounded-xl"
+            <ImageCheckbox
+              image={image}
+              onClick={() => handleImageClick(image)}
             />
           </div>
         ))}
